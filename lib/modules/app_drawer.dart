12b1c1b5dc47b14cf9109c472e7775292c080763
Rethik M <rethikmaheswaran@gmail.com>
2024-08-09 10:55:04 +0530

    update: Unwanted comments are removed

--- lib/modules/app_drawer.dart
+++ lib/modules/app_drawer.dart
@@ -16,28 +16,7 @@ class _AppDrawerState extends State<AppDrawer> {
   @override
   Widget build(BuildContext context) {
     // final List<Item> items = [
-    //   // Item(name: "Home", icon: Icons.home),
-    //   Item(name: "LR History", icon: Icons.history),
-    //   Item(name: "Assignment List", icon: Icons.list),
-    //   Item(name: "GDM", icon: Icons.local_shipping),
-    //   Item(name: "Create LR", icon: Icons.receipt_long)
-    // ];
-    // return Drawer(
-    //   backgroundColor: Colors.white,
-    //   child: ListView.builder(
-    //     itemCount: items.length,
-    //     itemBuilder: (context, index) {
-    //       return ListTile(
-    //         leading: Icon(items[index].icon),
-    //         title: Text(items[index].name),
-    //         onTap: () {
-    //           Navigator.push(context,
-    //           MaterialPageRoute(builder: (context) => DocList(title: items[index].name)));
-    //         },
-    //       );
-    //     },
-    //   ),
-    // );
+    
     return Drawer(
       // backgroundColor: Colors.white,
       child: SafeArea(
