800c42ca4740a9db7299a13c7c0a8bde7a28a7ca
Sudharsanan11 <sudharsananashok1975@gmail.com>
2025-04-10 12:39:00 +0530

    feat: add some endpoits

--- lib/api_endpoints.dart
+++ lib/api_endpoints.dart
@@ -1,7 +1,7 @@
 class ApiEndpoints {
-  // static const String baseUrl = "https://eff.4csolutions.in";
+  static const String baseUrl = "https://eff.4csolutions.in";
   // static const String baseUrl = "http://192.168.31.208:8000";
-  static const String baseUrl = "http://192.168.1.13:8000";
+  // static const String baseUrl = "http://192.168.1.2:8000";
   static _AuthEndPoints authEndpoints = _AuthEndPoints();
 }
 
@@ -31,13 +31,13 @@ class _AuthEndPoints {
   final String vehicleLog = '/api/resource/Vehicle Log';
   final String tripLog = '/api/method/logistics.pms.doctype.collection_assignment.collection_assignment.make_vehicle_log';
   final String gdmTripLog = '/api/method/logistics.pms.doctype.gdm.gdm.make_trip_log';
-  final String gdmloadingDetails = '/api/method/logistics.pms.doctype.gdm.gdm.get_loading_deatils';
+  final String gdmloadingDetails = '/api/method/logistics.whitelist_api.get_loading_deatils';
   final String returnLog = '/api/method/logistics.pms.doctype.collection_assignment.collection_assignment.make_vehicle_return';
   final String loadingDetails = '/api/resource/Loading Details';
-  final String gdmDelivery = '/api/method/logistics.pms.doctype.gdm.gdm.get_delivery_details';
+  final String gdmDelivery = '/api/method/logistics.whitelist_api.get_delivery_details';
   final String gdmReturnLog = '/api/method/logistics.pms.doctype.gdm.gdm.make_vehicle_return';
-  final String branchDelivery = '/api/method/logistics.pms.doctype.gdm.gdm.get_branch_delivery_details';
-  final String getUnloadingDetails = '/api/method/logistics.pms.doctype.collection_assignment.collection_assignment.make_unloading_details';
+  final String branchDelivery = '/api/method/logistics.whitelist_api.get_branch_delivery_details';
+  final String getUnloadingDetails = '/api/method/logistics.whitelist_api.make_unloading_details';
   final String fetchLR = '/api/method/logistics.pms.doctype.gdm.gdm.get_lrs';
   final String setPMChecklist = '/api/method/logistics.api.update_pm_checklist';
   final String unLoadingDetails = '/api/resource/Unloading Details';
